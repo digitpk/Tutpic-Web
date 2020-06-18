d8f7434e8edb25b02fd0dbd3bd1f667d7c73754b
Ali Bin Ajmal <alibinajmal@gmail.com>
2020-06-18 22:40:56 +0500

    delete old files

--- resources/views/auth/includes/social-links.blade.php
+++
@@ -1,3 +0,0 @@
-<a class="btn btn-lg btn-primary btn-block btn-grad text-uppercase fb"
-   type="submit">
-    <i class="fab fa-facebook-f"></i> Facebook</a>
--- resources/views/mail/chat/new.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-# New Chat Session
-Please Click Below to join the session
-@component('mail::button', ['url' => url($data['url'])])
-join session
-@endcomponent
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
