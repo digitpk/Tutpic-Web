9fafb8c032ec4b4bbe8ef2a962dd374fe709f100
Ali Bin Ajmal <alibinajmal@gmail.com>
2020-06-18 22:39:59 +0500

    clean old files

--- resources/views/layout/includes/alerts.blade.php
+++
@@ -1,5 +0,0 @@
-
-<div style="display: none" id="alert-new-chat" class="alert alert-success alert-dismissible">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>New Session!</strong> <a href="#" id="new-chat-link">Click here</a> to join new session
-</div>
